f7cb155dfb9bd3308bc53e89d08929cd76ceb590
Dariusz <darek200180@gmail.com>
2020-08-05 20:16:49 +0100

    add box component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Box from './Box';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>      
+
+      <Box width={10} height={15} color="red" />
+      <Box width={5} height={8} color="green" />
     </div>
   );
 }
