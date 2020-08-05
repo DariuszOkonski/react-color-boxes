7c3ae499a07c4c59f55ea8dc688e33c0b2cc27a3
Dariusz <darek200180@gmail.com>
2020-08-05 20:35:55 +0100

    add NewBoxForm component

---
+++ src/NewBoxForm.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+
+class NewBoxForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            height: "",
+            width: "",
+            color: ""
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        });
+    }
+
+    handleSubmit(evt) {
+        evt.preventDefault();
+
+        this.setState({
+            height: 0,
+            width: 0,
+            color: ""
+        })
+    }
+
+    render() { 
+        return ( 
+            <form>
+                <div>
+                    <label htmlFor="height">Height: </label>
+                    <input 
+                        type="text" 
+                        id="height"
+                        name="height"
+                        onChange={this.handleChange}
+                        value={this.state.height}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="width">Width: </label>
+                    <input 
+                        type="text"
+                        id="width"
+                        name="width"
+                        onChange={this.handleChange}
+                        value={this.state.width}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="color">Color: </label>
+                    <input 
+                        type="text"
+                        id="color"
+                        name="color"
+                        onChange={this.handleChange}
+                        value={this.state.color}
+                    />
+                </div>
+                <button>Add Square!</button>
+            </form>
+         );
+    }
+}
+ 
+export default NewBoxForm;
