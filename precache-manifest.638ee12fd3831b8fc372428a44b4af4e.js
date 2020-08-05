fc529be9531822951797ef66f9004cab644c2cec
Dariusz <darek200180@gmail.com>
2020-08-05 20:09:47 +0100

    Updates

---
+++ precache-manifest.638ee12fd3831b8fc372428a44b4af4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ee65539ff6fabbb57e07a86b9a352e",
+    "url": "/react-color-boxes/index.html"
+  },
+  {
+    "revision": "99d87ed46b083875a6ee",
+    "url": "/react-color-boxes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "879851efeed912b0bcb3",
+    "url": "/react-color-boxes/static/js/2.0680e083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-color-boxes/static/js/2.0680e083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d87ed46b083875a6ee",
+    "url": "/react-color-boxes/static/js/main.9e78dabc.chunk.js"
+  },
+  {
+    "revision": "04b23d5368816f84d562",
+    "url": "/react-color-boxes/static/js/runtime-main.8fe3027a.js"
+  }
+]);
